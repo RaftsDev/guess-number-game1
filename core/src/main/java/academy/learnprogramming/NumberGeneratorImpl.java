070044c86d90cc20425ffc12139e90b99347cd5d
Ralph <raftsdev@gmail.com>
2022-07-21 11:15:15 -0400

    Feat: Add impl class.

---
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class NumberGeneratorImpl implements NumberGenerator {
+    @Override
+    public int next() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return 0;
+    }
+}
