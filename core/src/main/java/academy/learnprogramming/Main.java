0412cc2a4f1a0173329629248c77910eadf44249
Ralph <raftsdev@gmail.com>
2022-07-21 11:08:10 -0400

    Feat: Add logger.

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -1,4 +1,12 @@
 package academy.learnprogramming;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess the number game");
+    }
 }
