d86814259dfbff588b8773fe3f77c2751a9da4fd
Ralph <raftsdev@gmail.com>
2022-07-21 12:01:43 -0400

    Feat: Create code of implementation with beans.

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,28 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private final static Logger log = LoggerFactory.getLogger(Main.class);
+    private final static String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess the number game");
+
+        //create context container
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        //Call method next() to get random number
+        int number = numberGenerator.next();
+
+        //log generated number
+        log.info("number = {}",number);
+
+        // close container
+        context.close();
     }
 }
