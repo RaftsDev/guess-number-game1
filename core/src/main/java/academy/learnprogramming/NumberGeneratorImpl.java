c762db297d563d61ceb48c45871cc1bc7f3dd2ed
Ralph <raftsdev@gmail.com>
2022-07-21 11:32:41 -0400

    Feat: Finish impl class.

--- core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,13 +1,22 @@
 package academy.learnprogramming;
 
+import java.util.Random;
+
 public class NumberGeneratorImpl implements NumberGenerator {
+
+    //== fields ==
+
+    private final Random random = new Random();
+    private int MaxNumber = 100;
+
+    //== public methods ==
     @Override
     public int next() {
-        return 0;
+        return random.nextInt(MaxNumber);
     }
 
     @Override
     public int getMaxNumber() {
-        return 0;
+        return MaxNumber;
     }
 }
